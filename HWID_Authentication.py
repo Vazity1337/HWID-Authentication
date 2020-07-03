5184ef85dbf85d0710518173269124f41d5fe129
Vazity <58231563+Vazity1337@users.noreply.github.com>
2020-07-02 21:26:36 -0400

    Update HWID_Authentication.py

--- HWID_Authentication.py
+++ HWID_Authentication.py
@@ -1,14 +1,14 @@
 import subprocess, requests, time, os
 
 hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
-r = requests.get('url here') # The link where you store your HWIDS e.g. a raw pastebin
+r = requests.get('url here')
 
 try:
     if hwid in r.text:
         pass
     else:
         print('[ERROR] HWID Not in database')
-        print(f'HWID: {hwid}') # prints HWID for user to copy
+        print(f'HWID: {hwid}') 
         time.sleep(5)
         os._exit()
 except:
@@ -19,3 +19,4 @@ except:
 os.system('title HWID Authentication Demo')
 
 print('Welcome')
+input()
